545ad726aa716c181368ba75e436cd238281d22d
awssd8080 <awssreekanth04@gmail.com>
2025-08-20 15:45:26 -0400

    Update 001-create-table.sql

--- db/changes/001-create-table.sql
+++ db/changes/001-create-table.sql
@@ -1,3 +1,25 @@
--- liquibase formatted sql
--- changeset sreekanth:create_test_table
-CREATE TABLE IF NOT EXISTS test_table (id SERIAL PRIMARY KEY, name TEXT);
+--liquibase formatted sql
+
+--changeset liquibase_user:001
+CREATE TABLE IF NOT EXISTS test_table (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+--changeset liquibase_user:002
+CREATE TABLE IF NOT EXISTS test_table2 (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+--changeset liquibase_user:003
+CREATE TABLE IF NOT EXISTS test_table3 (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+--changeset liquibase_user:004
+CREATE TABLE IF NOT EXISTS test_table4 (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
